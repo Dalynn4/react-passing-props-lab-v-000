6b1117acec7f06d0dd966b64c365207be3e1bf0f
Randall Robinson <keith_adams007@yahoo.com>
2018-07-30 18:03:21 +0000

    Done.

--- test/Filter_test.js
+++ test/Filter_test.js
@@ -22,35 +22,5 @@ describe('<Filter />', () => {
       shallow(<Noop />) : shallow(<Filter filters={filters} handleChange={spy}/>);
   });
 
-  it('should be a stateless functional component', () => {
-    const tryToGetState = () => { wrapper.state(); }
-    expect(tryToGetState).to.throw(
-      'ShallowWrapper::state() can only be called on class components',
-      'Component should not have state.'
-    );
-  });
 
-  it('should have a defaultProp "filters"', () => {
-    expect(Filter.defaultProps, 'defaultProps is not defined.').to.exist;
-    expect(Filter.defaultProps).to.have.any.keys('filters');
-  });
-
-  it('should have a defaultProp "handleChange"', () => {
-    expect(Filter.defaultProps, 'defaultProps is not defined.').to.exist;
-    expect(Filter.defaultProps).to.have.any.key('handleChange');
-  });
-
-  it('should call "handleChange" callback when there is a change', () => {
-    wrapper.find('select').simulate('change');
-    expect(spy.calledOnce).to.be.true;
-  });
-
-  it('should render a select element with a default option "all"', () => {
-    expect(wrapper.find('select').childAt(0).prop('value')).to.equal('all');
-  });
-
-  it('should render all the provided "filters"', () => {
-    const options = wrapper.find('option');
-    expect(options.length).to.equal(4, 'Did not render all the filters.');
-  });
 });
